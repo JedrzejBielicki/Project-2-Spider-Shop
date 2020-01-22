20360d620c3e460e8caf0b4bc5d91634709e9179
Jędrzej <jedrzejb123@gmail.com>
2020-01-22 21:39:29 +0100

    add parallax efect on header

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var button = document.querySelector("button");
-var header = document.querySelectorAll("h1");
-
-button.addEventListener("click", function () {
-    console.log("test");
-});
-
-header.addEventListener("click", function () {
-    console.log("test");
+function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateX(${distance * speed}px)`;
+}
+window.addEventListener("scroll", function () {
+    parallax(".first", window.scrollY, 1);
+    parallax(".second", window.scrollY, 0.9);
+    parallax(".third", window.scrollY, 0.8);
+    parallax(".fourth", window.scrollY, 0.7);
 })
