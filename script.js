d7a98b1368e6dc8c8cc1425bfed54389ed8a51d7
Jędrzej <jedrzejb123@gmail.com>
2020-01-22 14:18:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.querySelector("button");
+var header = document.querySelectorAll("h1");
+
+button.addEventListener("click", function () {
+    console.log("test");
+});
+
+header.addEventListener("click", function () {
+    console.log("test");
+})
