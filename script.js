0692c8b793f4ace9625bf05b6f4db02f27aa1321
Jędrzej Bielicki <jedrzejb123@gmail.com>
2020-02-03 12:02:33 +0100

    style.css add new class header-none script.js fix animation on header

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ function parallax(element, distance, speed) {
 }
 window.addEventListener("scroll", function () {
     parallax(".first", window.scrollY, 1);
-    parallax(".second", window.scrollY, 0.9);
+    parallax(".second", window.scrollY, 0.6);
     parallax(".third", window.scrollY, 0.8);
     parallax(".fourth", window.scrollY, 0.7);
+})
+
+let hero = document.querySelector(".hero");
+let header = document.querySelector(".header");
+let height = hero.clientHeight;
+
+
+
+document.addEventListener('scroll', function (){
+    if(scrollY > height){
+        header.classList.add('header-none');  
+    }
+    else{
+        header.classList.remove('header-none');
+    }
 })
